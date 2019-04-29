d75227955968fe8f7fed5ed24933fc659a688e4b
DaCreasy <dacreasy@ursinus.edu>
2019-04-29 14:23:33 -0400

    Basic TCP Server echo

---
+++ TCPserver.py
@@ -0,0 +1,17 @@
+from socket import *
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+severSocket.bind(('',serverPort))
+serverSocket.listen(1)
+print("Server is ready and listening for connections...")
+while 1:
+    connectionSocket, addr = serverSocket.accept()
+    print("Connection established...")
+    sentence = connectionSocket.recv(1024)
+    print("Message received: ", sentence)
+    print("Sending echo...")
+    connectionSocket.send(sentence)
+    print("Echo sent. Closing connection...")
+    connectionSocket.close()
+    print("Connection closed...")
