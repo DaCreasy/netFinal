a91afd3f875c5ad436bb5ae6787dc860f6ebc0c5
tyarosh <tiyarosh@ursinus.edu>
2019-04-29 14:30:56 -0400

    Update TCPclient.py
    Basic TCP client connection

--- TCPclient.py
+++ TCPclient.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+ author Damian Creasy
+ author Tim Yarosh
+ version 1.0
+ date 4/29/19
+ section CS475
+ 
+ Basic TCP client class. 
+ Source: Computer Networking a Top Down Approach 6th Edition 
 """
 
+from socket import *
+serverName = '172.25.43.182' #IP address of Damian's computer acting as our server
+serverPort = 12000 #arbitrary port #
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = raw_input("Input lowercase sentence:")
+clientSocket.send(sentence)
+echo = clientSocket.recv(1024)
+print('From Server:', echo)
+clientSocket.close()
