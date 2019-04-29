3ffccaa42dbabdafa1c59c9948ccb8f01d4ccdc2
tyarosh <tiyarosh@ursinus.edu>
2019-04-29 17:05:05 -0400

    Update TCPclient.py

--- TCPclient.py
+++ TCPclient.py
@@ -3,7 +3,6 @@
  author Damian Creasy
  author Tim Yarosh
  version 1.0
- date 4/29/19
  section CS475
  
  Basic TCP client class. 
@@ -11,20 +10,36 @@
 """
 
 import socket
-serverName = '172.25.43.182' #IP address of Damian's computer acting as our server
-serverPort = 12001 #arbitrary port #
+import threading
+import sys
+
+isConnected = False
+serverIP = '172.25.43.182' #IP address of Damian's computer acting as our server
+portNum = 12003 #arbitrary port #
+
+def sendMsg(clientSocket):
+        msg = input('Enter message: ')
+        encoded_msg = str.encode(msg)
+        clientSocket.send(encoded_msg)
+
+def receiveMsg(clientSocket):
+        msg = clientSocket.recv(1024)
+        print(msg.decode())
+
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-    
-sentence = input("Input lowercase sentence:")
-encoded = str.encode(sentence)
 
 try:
-    clientSocket.send(encoded)
-except Exception:
+    clientSocket.connect((serverIP,portNum))
+    print('Connection successful')
+    isConnected = True
+    
+    sendMsg(clientSocket)
+    receiveMsg(clientSocket)
+    
     clientSocket.close()
-
-echo = clientSocket.recv(64)
-print('From Server:', echo.decode())
-clientSocket.close()
+    
+except Exception as e:
+    print('Failed to connect:' + ' ' + str(e))
+    sys.exit(0)
+    
