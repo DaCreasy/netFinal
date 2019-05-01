e43f926f9f55245843ae35bc0296257c4c748b98
DaCreasy <dacreasy@ursinus.edu>
2019-04-30 22:00:21 -0400

    Add threading to server

--- README.md
+++ README.md
@@ -2,9 +2,5 @@
 
 TODO:
 
-Timer on server side
-Threading to allow multiple messages (Traces or something else to kill threads)
 Pickle username and message (or create user account on server side?)
 Multiple users connection simultaneously
-Ensure connections close on ALL sides in force close
-Manage IP
--- TCPserver.py
+++ TCPserver.py
@@ -1,17 +1,56 @@
 from socket import *
+from datetime import datetime
 
-serverPort = 12003
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('',serverPort))
-serverSocket.listen(1)
+welcome = "You've successfully connected to the room! Welcome!"
+
+def newClient(client, addr):
+    
+    # Welcome the newly connected client
+    client.send(str.encode(welcome))
+    
+    # Begin looping for receiving client messages
+    while 1:
+        try:
+            clientMSG = client.recv(1024)
+            if clientMSG:
+                # Log the client message and the time at which it is received
+                logMSG = "User["+ addr[0] +"] @ ["+datetime.now()+"] " + clientMSG.decode()
+                print(logMSG)
+                
+                for user in users:
+                    if user != client:
+                        try:
+                            user.send(str.encode(logMSG))
+                        except:
+                            user.close()
+                            users.remove(user)
+            else:
+                users.remove(client)
+        except:
+            continue
+                            
+serverPort = 12010
+server = socket(AF_INET, SOCK_STREAM)
+server.bind(('',serverPort))
+
+users = []
+server.listen(50)
 print("Server is ready and listening for connections...")
+
 while 1:
-    connectionSocket, addr = serverSocket.accept()
-    print("Connection established...")
-    sentence = connectionSocket.recv(1024)
-    print("Message received:", sentence.decode())
-    print("Sending echo...")
-    connectionSocket.send(sentence)
-    print("Echo sent. Closing connection...")
-    connectionSocket.close()
-    print("Connection closed...")
+
+    # Accept a connection with a new client
+    client, addr = server.accept()
+
+    # Add the client to the list of active users
+    users.append(client)
+
+    # Log the new user
+    print("User["+ addr[0] +"] has joined...")
+    
+    # Start a new thread for the new user and begin listening for messages
+    clientThread = Thread(target = newClient, args = (client,addr,), daemon = True)
+    clientThread.start()
+    
+client.close()
+server.close()
